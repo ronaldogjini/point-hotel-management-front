311bb17a2d350d1d850cff8997abf1af3dbe998f
ronaldogjini <ronaldogjini@users.noreply.github.com>
2021-08-05 16:24:50 +0200

    New source api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const ax = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'https://point-hotel-management-back.herokuapp.com/api/',
     headers: {
         Accept: "application/json"
     },
